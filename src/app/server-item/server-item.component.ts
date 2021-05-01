e5c00e156a34100567f6e1fc8c2abf5175fab7a1
Waqas <wakassadik@gmail.com>
2021-05-01 10:27:29 +0500

    adding alias to @input() element to use it in app component

--- src/app/server-item/server-item.component.ts
+++ src/app/server-item/server-item.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ServerItemComponent implements OnInit {
 
-  @Input() element: {type: string, name: string, content: string};
+  @Input("srvElement") element: {type: string, name: string, content: string};
 
 
   constructor() { }
